d5f3c98b4dd167e1a956a59a7b0bbd79d3c3f59c
Utsav Singla <43605886+singlautsav@users.noreply.github.com>
2024-11-12 23:19:20 +0530

    ton integration success

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ import { BiWalletAlt } from "react-icons/bi";
 import { IoMdNotificationsOutline } from "react-icons/io";
 import LoginButton from "./LoginButton";
 import toast, { Toaster } from "react-hot-toast";
-// import ConnectWallet from "./LoginWithTonButton"
+import ConnectWallet from "./LoginWithTonButton"
 // import { TonConnectButton } from "@tonconnect/ui-react";
 
 export default function Header() {
@@ -19,8 +19,8 @@ export default function Header() {
           <span className="text-[12px]">Hello, Username</span>
         </div>
         <div className="flex ">
-          {/* <ConnectWallet /> */}
-          <LoginButton/>
+          <ConnectWallet />
+          {/* <LoginButton/> */}
          
         {/* <button className="text-3xl"><IoMdNotificationsOutline/></button>
         <button className="text-white text-2xl px-3 py-2 bg-slate-800 rounded-full"><BiWalletAlt /></button> */}
